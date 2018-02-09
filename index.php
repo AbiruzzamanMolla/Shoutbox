55e51a62d8a95e056ebf197599c3dc996dbd8639
asliabir <abiruzzaman.molla@gmail.com>
2018-02-09 23:03:52 +0600

    HTML Structure CSS Styleing Done!

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Shout Box</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="styles/main.css" />
+    <script src="javascripts/main.js"></script>
+</head>
+
+<body>
+    <div class="wrapper clr">
+        <header class="headsection clr">
+            <h2>
+                ShoutBOX with PHP & MySQLi
+            </h2>
+        </header>
+        <section class="content clr">
+            <div class="box">
+                <ul>
+                    <li><span>05: 30 pm</span> - <b>Abir</b> Hello, I am new here!</li>
+                    <li><span>05: 30 pm</span> - <b>Abir</b> Hello, I am new here!</li>
+                    <li><span>05: 30 pm</span> - <b>Abir</b> Hello, I am new here!</li>
+                    <li><span>05: 30 pm</span> - <b>Abir</b> Hello, I am new here!</li>
+                    <li><span>05: 30 pm</span> - <b>Abir</b> Hello, I am new here!</li>
+                    <li><span>05: 30 pm</span> - <b>Abir</b> Hello, I am new here!</li>
+                    <li><span>05: 30 pm</span> - <b>Abir</b> Hello, I am new here!</li>
+                    <li><span>05: 30 pm</span> - <b>Abir</b> Hello, I am new here!</li>
+                    <li><span>05: 30 pm</span> - <b>Abir</b> Hello, I am new here!</li>
+                    <li><span>05: 30 pm</span> - <b>Abir</b> Hello, I am new here!</li>
+                    <li><span>05: 30 pm</span> - <b>Abir</b> Hello, I am new here!</li>
+                </ul>
+            </div>
+            <div class="shoutform clr">
+                <form action="">
+                    <table>
+                        <tr>
+                            <td>Name</td>
+                            <td>:</td>
+                            <td><input type="text" name="name" required placeholder="Enter your message"/></td>
+                        </tr>
+                        <tr>
+                            <td>Message</td>
+                            <td>:</td>
+                            <td><input type="text" name="msg" required placeholder="Enter your message"/></td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td><input type="submit" value="Shout It"></td>
+                        </tr>
+                    </table>
+                </form>
+            </div>
+        </section>
+        <footer class="footsection clr">
+            <h2>&copy; Copyright with Abir's Project</h2>
+        </footer>
+    </div>
+</body>
+
+</html>
